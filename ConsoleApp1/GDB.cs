9b9bef2b42297f382817f83a4604dbabaab30171
Tomas Ukrinas <tomas.ukrinas1997@gmail.com>
2018-11-18 16:43:08 +0200

    Protokolas

--- ConsoleApp1/GDB.cs
+++ ConsoleApp1/GDB.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
 {
     class Projekcija
     {
+        public int Index { get; set; }
         public List<char> Reikalavimai { get; set; }
         public char Rezultatas { get; set; }
         public int Flag { get; set; }
@@ -15,6 +16,19 @@ namespace ConsoleApp1
         {
             Reikalavimai = new List<char>();
             Flag = 0;
+        }
+        public override string ToString()
+        {
+            string output = "R";
+            output += this.Index.ToString();
+            output += String.Format(":{0}", this.Reikalavimai.ElementAt(0));
+            for (int i = 1; i < this.Reikalavimai.Count; i++)
+            {
+                output += String.Format(",{0}", this.Reikalavimai.ElementAt(i));
+            }
+            output += "->";
+            output += this.Rezultatas;
+            return output;
         }
     }
     class GDB
@@ -30,6 +44,7 @@ namespace ConsoleApp1
         public char Tikslas { get; set; }
 
         public List<Projekcija> Kelias { get; set; }
+        public String FaktaiToString { get { return String.Format("{0} ir {1}", CharListToString(InitFaktai), CharListToString(Faktai)); } }
         public GDB()
         {
             Projekcijos = new List<Projekcija>();
@@ -42,18 +57,21 @@ namespace ConsoleApp1
             Projekcijos = new List<Projekcija>();
 
             Projekcija p = new Projekcija();
+            p.Index = 1;
             p.Reikalavimai.Add('F');
             p.Reikalavimai.Add('B');
             p.Rezultatas = 'Z';
             Projekcijos.Add(p);
 
             p = new Projekcija();
+            p.Index = 2;
             p.Reikalavimai.Add('C');
             p.Reikalavimai.Add('D');
             p.Rezultatas = 'F';
             Projekcijos.Add(p);
 
             p = new Projekcija();
+            p.Index = 3;
             p.Reikalavimai.Add('A');
             p.Rezultatas = 'D';
             Projekcijos.Add(p);
@@ -66,6 +84,21 @@ namespace ConsoleApp1
             Faktai = new List<char>();
             Tikslas = 'Z';
             Kelias = new List<Projekcija>();
+        }
+
+        private String CharListToString(List<char> list)
+        {
+            string output;
+            if (list.Count > 0)
+            {
+                output = list.ElementAt(0).ToString();
+                for (int i = 1; i < list.Count; i++)
+                {
+                    output += String.Format(", {0}", list.ElementAt(i));
+                }
+            }
+            else output = "";
+            return output;
         }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -10,15 +12,49 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int iCount = 0; //Iteraciju skaitliukas
+
             GDB db = new GDB(true);
+
+            #region Protokolas 1 dalis
+            string output;
+            StreamWriter file = new StreamWriter(ConfigurationManager.AppSettings["ProtocolFileName"], false);
+            file.WriteLine("1 DALIS. Duomenys");
+            file.WriteLine("");
+            file.WriteLine("  1) Taisyklės");
+            foreach (Projekcija p in db.Projekcijos)
+            {
+                file.WriteLine(String.Format("    {0}", p));
+            }
+            file.WriteLine("");
+            file.WriteLine("  2) Faktai");
+            output = String.Format("    {0}", db.InitFaktai.ElementAt(0));
+            for (int i = 1; i < db.InitFaktai.Count; i++)
+            {
+                output += String.Format(", {0}", db.InitFaktai.ElementAt(i));
+            }
+            file.WriteLine(output);
+            file.WriteLine("");
+            file.WriteLine("  2) Tikslas");
+            file.WriteLine(String.Format("    {0}", db.Tikslas));
+            file.WriteLine("");
+            #endregion
 
             #region Vykdymas
             if (db.InitFaktai.Contains(db.Tikslas))
             {
-
+                file.WriteLine("3 DALIS. Rezultatai");
+                file.WriteLine(String.Format("  Tikslas {0} tarp faktų. Kelias tuščias.", db.Tikslas));
+                file.Close();
+                return;
             }
+            file.WriteLine("2 DALIS. Vykdymas");
+            file.WriteLine("");
             while (!db.VisiFaktai.Contains(db.Tikslas))
             {
+                iCount++;
+                file.WriteLine(String.Format("  {0} ITERACIJA", iCount.ToString()));
+
                 bool done = false;
                 foreach(Projekcija p in db.Projekcijos)
                 {
@@ -27,26 +63,58 @@ namespace ConsoleApp1
                         if (db.VisiFaktai.Contains(p.Rezultatas))
                         {
                             p.Flag = 2;
-                        }
-                        bool tinka = true;
-                        foreach (char reikalavimas in p.Reikalavimai)
+                            file.WriteLine(String.Format("    {0} netaikome, nes konsekventas faktuose. Pakeliame flag2", p));
+                        } else
                         {
-                            if (!db.VisiFaktai.Contains(reikalavimas))
+                            bool tinka = true;
+                            String trukstamas = "";
+                            foreach (char reikalavimas in p.Reikalavimai)
                             {
-                                tinka = false;
+                                if (!db.VisiFaktai.Contains(reikalavimas))
+                                {
+                                    tinka = false;
+                                    if(trukstamas == "")
+                                    {
+                                        trukstamas += reikalavimas;
+                                    } else
+                                    {
+                                        trukstamas += ", " + reikalavimas;
+                                    }
+                                }
+                            }
+                            if (tinka)
+                            {
+                                db.Faktai.Add(p.Rezultatas);
+                                p.Flag = 1;
+                                db.Kelias.Add(p);
+                                done = true;
+                                file.WriteLine(String.Format("    {0} taikome. Pakeliame flag1. Faktai {1}.", p, db.FaktaiToString));
+                            } else
+                            {
+                                file.WriteLine(String.Format("    {0} netaikome, nes trūksta {1}.", p, trukstamas));
                             }
                         }
-                        if (tinka)
-                        {
-                            db.Faktai.Add(p.Rezultatas);
-                            p.Flag = 1;
-                            db.Kelias.Add(p);
-                            done = true;
-                        }
+                    } else if (p.Flag != 0 && !done)
+                    {
+                        file.WriteLine(String.Format("    {0} praleidžiame, nes pakelta flag{1}", p, p.Flag.ToString()));
                     }
                 }
+                file.WriteLine("");
             }
             #endregion
+
+            #region Rezultatai
+            file.WriteLine("3 DALIS. Rezultatai");
+            file.WriteLine(String.Format("  Tikslas {0} išvestas.", db.Tikslas));
+            output = "R" + db.Kelias.ElementAt(0).Index.ToString();
+            for (int i = 1; i < db.Kelias.Count; i++)
+            {
+                output += String.Format(", R{0}", db.Kelias.ElementAt(i).Index.ToString());
+            }
+            file.WriteLine(String.Format("  Kelias: {0}.", output));
+            #endregion
+
+            file.Close();
         }
     }
 }
