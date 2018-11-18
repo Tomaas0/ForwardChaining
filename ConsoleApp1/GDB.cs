db443dbbb9c499821aad8582c3d6eeaa10b2544c
Tomas Ukrinas <tomas.ukrinas1997@gmail.com>
2018-11-18 13:35:15 +0200

    GDB added

---
+++ ConsoleApp1/GDB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Projekcija
+    {
+        public List<char> Reikalavimai { get; set; }
+        public char Rezultatas { get; set; }
+        public int Flag { get; set; }
+        public Projekcija()
+        {
+            Reikalavimai = new List<char>();
+            Flag = 0;
+        }
+    }
+    class GDB
+    {
+        public List<Projekcija> Projekcijos { get; set; }
+        public List<char> InitFaktai { get; set; }
+        public List<char> Faktai { get; set; }
+        public char Tikslas { get; set; }
+        public GDB()
+        {
+            Projekcijos = new List<Projekcija>();
+            InitFaktai = new List<char>();
+            Faktai = new List<char>();
+        }
+        public GDB(bool x)
+        {
+            Projekcijos = new List<Projekcija>();
+            Projekcija p = new Projekcija();
+
+            p.Reikalavimai.Add('F');
+            p.Reikalavimai.Add('B');
+            p.Rezultatas = 'Z';
+            Projekcijos.Add(p);
+
+            p.Reikalavimai.Add('C');
+            p.Reikalavimai.Add('D');
+            p.Rezultatas = 'F';
+            Projekcijos.Add(p);
+
+            p.Reikalavimai.Add('A');
+            p.Rezultatas = 'D';
+            Projekcijos.Add(p);
+
+            InitFaktai = new List<char>();
+            InitFaktai.Add('A');
+            InitFaktai.Add('B');
+            InitFaktai.Add('C');
+
+            Faktai = new List<char>();
+            Tikslas = 'Z';
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            GDB db = new GDB(true);
         }
     }
 }
